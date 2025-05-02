ee0f99b6c69f4d2f47fe165a29cacc82db50fae3
Andreas249964 <249964@usn.no>
2025-05-02 15:12:33 +0200

    fixed duplicate r3eesources

--- Vms/VMs.tf
+++ Vms/VMs.tf
@@ -77,11 +77,11 @@ resource "openstack_compute_volume_attach_v2" "va_1" {
   instance_id = openstack_compute_instance_v2.Node1.id
   volume_id   = openstack_blockstorage_volume_v3.volume_1.id
 }
-resource "openstack_compute_volume_attach_v2" "va_1" {
+resource "openstack_compute_volume_attach_v2" "va_2" {
   instance_id = openstack_compute_instance_v2.Node2.id
   volume_id   = openstack_blockstorage_volume_v3.volume_2.id
 }
-resource "openstack_compute_volume_attach_v2" "va_1" {
+resource "openstack_compute_volume_attach_v2" "va_3" {
   instance_id = openstack_compute_instance_v2.Node3.id
   volume_id   = openstack_blockstorage_volume_v3.volume_3.id
 }
